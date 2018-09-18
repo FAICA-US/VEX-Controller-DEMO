315f2e2c444c56a2f783df2f1d9c3b47ca1a411e
FAICA-US <40411263+FAICA-US@users.noreply.github.com>
2018-09-17 20:13:49 -0400

    Update!

--- serialfunction.py
+++ serialfunction.py
@@ -1,8 +1,33 @@
 #!/usr/bin/env python
-   
+#Mon 17 Sep 2018 07:54:17 PM CDT
+# James Andrews
+# Bryan 
+
+#Authenticate with controller
+
+		#read serial code
+           ser = serial.Serial(
+              
+               port='/dev/ttyUSB0',
+               baudrate = 4800,
+               parity=serial.PARITY_NONE,
+               stopbits=serial.STOPBITS_ONE,
+               bytesize=serial.EIGHTBITS,
+               timeout=1
+           )
+           counter=0
+          
+      
+           while 1:
+               x=ser.readline()
+               print x
+
+
+          
+#Write serial code    
            import time
            import serial
-           
+          
       
            ser = serial.Serial(
               
@@ -15,34 +40,10 @@
            )
            counter=0
           
-#While 1, write counter
+      
            while 1:
-               ser.write('Write counter: %d'%(counter))
+               ser.write('Write counter: %d \n'%(counter))
                time.sleep(1)
                counter += 1
-
-#read AA 55 3B serial code
-
-           ser = serial.Serial(
-              
-               port='/dev/ttyUSB0',
-               baudrate = 4800,
-               parity=serial.PARITY_NONE,
-               stopbits=serial.STOPBITS_ONE,
-               bytesize=serial.EIGHTBITS,
-               timeout=1
-           )
-           counter=0
-          
-#look for AA 55 3B and reply with AA 55 39 0A RX RY LX LY 00      
-           while 1:
-               x=ser.readline()
-                while x=aa553b
-               ser.write('%d'%(counter))
-
-               time.sleep(1)                    
-#For Debugging               print x
-
-
 
 
